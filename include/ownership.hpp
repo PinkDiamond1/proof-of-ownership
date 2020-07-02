db848853ab8cbbd33954ec963bc804e4bbfca2ae
Andrew Chaney (netuoso) <andrewc@pobox.com>
2020-07-02 12:05:56 -0500

    code - checksum256, users table store data

--- include/ownership.hpp
+++ include/ownership.hpp
@@ -5,7 +5,14 @@ CONTRACT ownership : public contract {
    public:
       using contract::contract;
 
-      ACTION prove( name user, std::string code );
+      TABLE users {
+        name user;
+        checksum256 code;
 
+        uint64_t primary_key() const {return user.value;}
+      };
+      typedef eosio::multi_index<"users"_n, users> userstable;
+
+      ACTION prove( name user, checksum256 code );
       using prove_action = action_wrapper<"prove"_n, &ownership::prove>;
 };
--- src/ownership.cpp
+++ src/ownership.cpp
@@ -1,7 +1,21 @@
 #include <ownership.hpp>
 
-ACTION ownership::prove( name user, std::string code ) {
+ACTION ownership::prove( name user, checksum256 code ) {
 	require_auth(permission_level(user, "owner"_n));
 
+	userstable ustore(_self, user.value);
+
   print_f("Name : %\nCode : %\n",user,code);
+
+	if ( ustore.begin() == ustore.end() ) {
+    ustore.emplace(_self, [&](auto& u) {
+      u.user = user;
+      u.code = code;
+    });
+	} else {
+    ustore.modify(*ustore.begin(), _self, [&]( auto& u ) {
+      u.user = user;
+      u.code = code;
+    });
+	}
 }
