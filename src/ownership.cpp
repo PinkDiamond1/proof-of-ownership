c96173bf691ae21f689f21820209d1b75d9c0098
Andrew Chaney (netuoso) <andrewc@pobox.com>
2020-07-02 12:11:54 -0500

    whitespace

--- src/ownership.cpp
+++ src/ownership.cpp
@@ -1,21 +1,21 @@
 #include <ownership.hpp>
 
 ACTION ownership::prove( name user, checksum256 code ) {
-	require_auth(permission_level(user, "owner"_n));
+  require_auth(permission_level(user, "owner"_n));
 
-	userstable ustore(_self, user.value);
+  userstable ustore(_self, user.value);
 
   print_f("Name : %\nCode : %\n",user,code);
 
-	if ( ustore.begin() == ustore.end() ) {
+  if ( ustore.begin() == ustore.end() ) {
     ustore.emplace(_self, [&](auto& u) {
       u.user = user;
       u.code = code;
     });
-	} else {
+  } else {
     ustore.modify(*ustore.begin(), _self, [&]( auto& u ) {
       u.user = user;
       u.code = code;
     });
-	}
+  }
 }
